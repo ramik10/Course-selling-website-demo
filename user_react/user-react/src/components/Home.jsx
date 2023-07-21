bc2d985bc7bce178bf98e9e04acb921fa56f1477
RAMIK MUKHERJEE <ramik.14062000@gmail.com>
2023-07-21 23:56:01 +0530

    fixed npm problem

--- user_react/user-react/src/components/Home.jsx
+++ user_react/user-react/src/components/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 function Home(){
     return(
-    <div style={{paddingTop:"5vh"}}>
-        <h1>Homepage</h1>
-        <a href="/signup">register</a>
-        <br/>
-        <a href="/login">login</a>
-    </div>
+    <div style={{ display: "flex", justifyContent: "center", paddingTop: "5vh" }}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "50%",  borderRadius: "10px", padding: "20px" }}>
+            <h1>Homepage</h1>
+            <p>Click on the Courses tab to view the courses</p>
+            <br />
+            <p>Click on the Login tab to login</p>
+            <br />
+            <p>Click on the Signup tab to signup</p>
+        </div>
+        </div>
     )
 }
 export default Home;
